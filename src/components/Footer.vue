386b0913529920742a8a66b40eb10b37fbb2a6e0
Frédéric BECK <beck.frederic@gmail.com>
2018-11-01 17:40:37 +0100

    Modifie le footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,6 @@
         <div class="container-fluid">
             <div class="row-fluid">
                 <div class="col-md-12">
-                    Copyright &copy; 2016 Dong Liang - All rights reserved.
                 </div>
             </div>
         </div>

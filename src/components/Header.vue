3908626d65db891193171d00e097fa624992f630
Frédéric BECK <beck.frederic@gmail.com>
2018-11-03 12:07:06 +0100

    Ajoute le bouton de téléchargement du CV

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="header media">
-        <img class="align-self-start mr-3 d-none d-sm-block" src="../static/photo/photo.jpg" alt="Generic placeholder image">
-        <div class="media-body">
-            <h5 class="mt-0">{{ name }}</h5>
-            <h4>{{ poste }}</h4>
-            <div class="coordonnees">
-                <span><icon name="map-marker"></icon> {{ city }}</span>
-                <span class="break" />
-                <span v-if="email"><icon name="envelope" scale="1.2"></icon><a :href="'mailto:'+ email">{{ email }}</a></span>
-                <span v-if="social.github"><icon name="brands/github-square" scale="1.2"></icon><a title="GitHub" :href="'//github.com/'+ social.github" target="_blank">@{{ social.github }}</a></span>
-                <span v-if="social.linkedin"><icon name="brands/linkedin" scale="1.2"></icon><a title="LinkedIn" :href="'//linkedin.com/in/'+ social.linkedin" target="_blank">beckf</a></span>
-                <span class="break" />
-                <span v-if="other" class="saut">{{ other }}</span>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-10">
+                    <img class="align-self-start mr-3 d-none d-sm-block" src="../static/photo/photo.jpg" alt="Generic placeholder image">
+                    <div class="media-body">
+                        <h5 class="mt-0">{{ name }}</h5>
+                        <h4>{{ poste }}</h4>
+                        <div class="coordonnees">
+                            <span><icon name="map-marker"></icon> {{ city }}</span>
+                            <span class="break" />
+                            <span v-if="email"><icon name="envelope" scale="1.2"></icon><a :href="'mailto:'+ email">{{ email }}</a></span>
+                            <span v-if="social.github"><icon name="brands/github-square" scale="1.2"></icon><a title="GitHub" :href="'//github.com/'+ social.github" target="_blank">@{{ social.github }}</a></span>
+                            <span v-if="social.linkedin"><icon name="brands/linkedin" scale="1.2"></icon><a title="LinkedIn" :href="'//linkedin.com/in/'+ social.linkedin" target="_blank">beckf</a></span>
+                            <span class="break" />
+                            <span v-if="other" class="saut">{{ other }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-2 d-none d-sm-block align-self-end download-buttons">
+                    <a v-if="cv_pdf" :href="cv_pdf" type="button" class="btn btn-primary btn-sm">
+                        <icon :name="cv_icon"></icon> {{ cv_label }}
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -23,8 +34,9 @@
         props: ["name","poste","city","email","social","other"],
         data(){
             return {
-                cv_label: "CV",
-                cv_icon: "print"
+                cv_label: "Télécharger le CV",
+                cv_icon: "file-download",
+                cv_pdf: "../static/resume/cv.pdf"
             }
         }
     }
@@ -47,6 +59,7 @@
 
     .header img {
         height: 230px;
+        float: left;
     }
 
     .header li {
@@ -79,6 +92,17 @@
     .coordonnees span .fa-icon {
         color: #337ab7;
         margin-right: 5px;
+    }
+
+    .download-buttons > .btn {
+        float: right;
+        background-color: #337ab7;
+    }
+
+    .download-buttons > .btn > .fa-icon {
+        margin: 0px 3px 0px 0px;
+        bottom: 1px;
+        position: relative;
     }
 
 </style>
